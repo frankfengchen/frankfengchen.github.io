864a8ad016e1018a640821e69a128af92db9ec20
frankfengchen <fengchen.fc@gmail.com>
2017-03-28 23:45:16 -0500

    Practice cookie manipulation
    *Note: Chrome browser doesn't support local cookies (file cookies). You
    have to debug using a different browser.

---
+++ cookiefunc.js
@@ -0,0 +1,84 @@
+/* Please note that Chrome doesn't support local cookies so do NOT debug your
+   cookies related code on Chrome!!!
+*/
+
+//Add a new cookie
+function setCookie() {
+    var d = new Date();
+    var exdays = 1; //default expiration period is one day
+    var cname = document.getElementById("cookiename").value;
+    var cvalue = document.getElementById("cookievalue").value;
+
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+
+    createGreeting(); //update greetings
+    displayCookie(); //update all cookies display
+}
+
+//Find a cookie with the name "cname"
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+//display all current cookies (cookie expiration date isn't available through document.cookie)
+function displayCookie() {
+    var decodedCookie = decodeURIComponent(document.cookie);
+    //var ca = decodedCookie.split(';');
+    var allcookies = "<b>Current cookies list:</b> <i>";
+
+    allcookies += decodedCookie + "</i>";
+    document.getElementById("allcookies").innerHTML = allcookies;
+}
+
+//Display a greeting message based on "username" cookie
+function createGreeting() {
+    var user=getCookie("username");
+    var elem = document.getElementById("greeting");
+
+    elem.style.color = "blue";
+
+    if (user != "") {
+        elem.innerHTML = "Welcome again " + user + "!";
+    } else {
+        elem.innerHTML = "Welcome, new user!";
+    } 
+}
+
+//Erase a cookie
+function eraseCookie(cname = "") {
+    //Get the cookie name from form input if it's not specified
+    if(cname == "") 
+        cname = document.getElementById("cookiename").value;
+    document.cookie = cname + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+    createGreeting(); //update greetings
+    displayCookie(); //update all cookies display
+}
+
+//Erase all cookie
+function eraseAllCookie() {
+    var decodedCookie = decodeURIComponent(document.cookie); //Get all cookies
+    var ca = decodedCookie.split(/[=;]/); //split cookies to an array
+
+    //Get the cookie name from form input if it's not specified
+    for (var i = 0; i < ca.length; i+=2) {
+        eraseCookie(ca[i]);
+    }
+
+    createGreeting(); //update greetings
+    displayCookie(); //update all cookies display
+}
